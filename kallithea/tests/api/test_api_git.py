1cd03138461a167a8f2186f691ea678fc670a91d
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2015-04-22 01:10:29 +0200

    tests: avoid 'import *' in api tests

--- kallithea/tests/api/test_api_git.py
+++ kallithea/tests/api/test_api_git.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from kallithea.tests import *
+from kallithea.tests import TestController, GIT_REPO
 from kallithea.tests.api.api_base import _BaseTestApi
 
 
