2420b48ea6a95db3e133632a0c091df8a443524d
domruf <dominikruf@gmail.com>
2016-06-14 21:23:51 +0200

    hooks: set Windows stderr output mode to binary
    This prevents Python (or the Windows console) from replacing \n with  \r\n. The
    extra \r made exception output show up with empty lines.
    
    Assuming we only get text and never binary data on stderr, an alternative
    solution could be to strip trailing whitespace ...

--- kallithea/config/pre_receive_tmpl.py
+++ kallithea/config/pre_receive_tmpl.py
@@ -1,6 +1,15 @@
 import os
 import sys
 
+# set output mode on windows to binary for stderr
+# this prevents python (or the windows console) from replacing \n with  \r\n
+# git doesn't display remote output lines that contain \r
+# and therefore without this modification git would displayes empty lines
+# instead of the exception output
+if sys.platform == "win32":
+    import msvcrt
+    msvcrt.setmode(sys.stderr.fileno(), os.O_BINARY)
+
 KALLITHEA_HOOK_VER = '_TMPL_'
 os.environ['KALLITHEA_HOOK_VER'] = KALLITHEA_HOOK_VER
 from kallithea.lib.hooks import handle_git_pre_receive as _handler
