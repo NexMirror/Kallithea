9c472ab2ab3b002c1ee95e893d6835221122a720
Andrew Shadura <andrew@shadura.me>
2016-07-14 15:31:29 +0200

    pygrack: add missing author and copyright information
    This module is based on code from git_http_backend.py,
    adapted and integrated by Marcin Kuźmiński.
    
    The protocol request handling comes from Daniel, the
    rest of the code seems to come from Marcin.

--- kallithea/lib/middleware/pygrack.py
+++ kallithea/lib/middleware/pygrack.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+kallithea.lib.middleware.pygrack
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Python implementation of git-http-backend's Smart HTTP protocol
+
+Based on original code from git_http_backend.py project.
+
+Copyright (c) 2010 Daniel Dotsenko <dotsa@hotmail.com>
+Copyright (c) 2012 Marcin Kuzminski <marcin@python-works.com>
+
+This file was forked by the Kallithea project in July 2014.
+"""
+
 import os
 import socket
 import logging
