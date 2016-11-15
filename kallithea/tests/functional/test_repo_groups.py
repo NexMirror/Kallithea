7b023b5b8f97f00d7237efe96481463ab2dd3459
Mads Kiilerich <madski@unity3d.com>
2016-11-15 22:53:41 +0100

    tests: add some test coverage of repo group management

--- kallithea/tests/functional/test_repo_groups.py
+++ kallithea/tests/functional/test_repo_groups.py
@@ -12,6 +12,48 @@ class TestRepoGroupsController(TestController):
         self.log_user()
         response = self.app.get(url('new_repos_group'))
 
+    def test_create(self):
+        self.log_user()
+
+        group_name = 'foo'
+
+        # creation
+        response = self.app.post(url('repos_groups'),
+                                         {'group_name': group_name,
+                                         'group_description': 'lala',
+                                         'parent_group_id': '-1',
+                                         'group_copy_permissions': 'True',
+                                          '_authentication_token': self.authentication_token()})
+        self.checkSessionFlash(response, 'Created repository group %s' % group_name)
+
+        # edit form
+        response = self.app.get(url('edit_repo_group', group_name=group_name))
+        response.mustcontain('>lala<')
+
+        # edit
+        response = self.app.post(url('update_repos_group', group_name=group_name),
+                                         {'group_name': group_name,
+                                         'group_description': 'lolo',
+                                          '_authentication_token': self.authentication_token()})
+        self.checkSessionFlash(response, 'Updated repository group %s' % group_name)
+        response = response.follow()
+        response.mustcontain('name="group_name" type="text" value="%s"' % group_name)
+        response.mustcontain('>lolo<')
+
+        # listing
+        response = self.app.get(url('repos_groups'))
+        response.mustcontain('"totalRecords": 1')
+        response.mustcontain('raw_name": "%s"' % group_name)
+
+        # show
+        response = self.app.get(url('repos_group', group_name=group_name))
+        response.mustcontain('href="/_admin/repo_groups/%s/edit"' % group_name)
+
+        # delete
+        response = self.app.post(url('delete_repo_group', group_name=group_name),
+                                 {'_authentication_token': self.authentication_token()})
+        self.checkSessionFlash(response, 'Removed repository group %s' % group_name)
+
     def test_new_by_regular_user(self):
         self.log_user(TEST_USER_REGULAR_LOGIN, TEST_USER_REGULAR_PASS)
         response = self.app.get(url('new_repos_group'), status=403)
