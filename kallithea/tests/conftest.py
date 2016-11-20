211eabb647a84470329776c6bb50e1798bb88d2b
Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
2016-11-20 21:28:04 +0100

    tests: remove unused code from conftest.py
    conftest.py does not contain an import of pylons (only of pylons.test) so
    usage of e.g. pylons.config can never work, so it cannot be used.
    Since the tests run fine regardless, we can get rid of this code.

--- kallithea/tests/conftest.py
+++ kallithea/tests/conftest.py
@@ -23,15 +23,6 @@ def pytest_configure():
     pylons.test.pylonsapp = loadapp('config:kallithea/tests/test.ini', relative_to=path)
     logging.disable(logging.NOTSET)
 
-    # Setup the config and app_globals, only works if we can get
-    # to the config object
-    conf = getattr(pylons.test.pylonsapp, 'config')
-    if conf:
-        pylons.config._push_object(conf)
-
-        if 'pylons.app_globals' in conf:
-            pylons.app_globals._push_object(conf['pylons.app_globals'])
-
     # Initialize a translator for tests that utilize i18n
     translator = _get_translator(pylons.config.get('lang'))
     pylons.translator._push_object(translator)
