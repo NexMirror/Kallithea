2eaed9a80e39fea85a5c0cae2674d51b16b4ec45
Mads Kiilerich <mads@kiilerich.com>
2017-01-03 02:06:41 +0100

    middleware: drop gzipper middleware
    It added unfortunate buffering, was only enabled for static_files=true, and
    isn't available for TurboGears2.
    
    If necessary, it is better to configure the web server to provide it.

--- kallithea/config/middleware.py
+++ kallithea/config/middleware.py
@@ -20,7 +20,6 @@ from paste.cascade import Cascade
 from paste.registry import RegistryManager
 from paste.urlparser import StaticURLParser
 from paste.deploy.converters import asbool
-from paste.gzipper import make_gzip_middleware
 
 from pylons.middleware import ErrorHandler, StatusCodeRedirect
 from pylons.wsgiapp import PylonsApp
@@ -105,7 +104,6 @@ def make_app(global_conf, full_stack=True, static_files=True, **app_conf):
         # Serve static files
         static_app = StaticURLParser(config['pylons.paths']['static_files'])
         app = Cascade([static_app, app])
-        app = make_gzip_middleware(app, global_conf, compress_level=1)
 
     app.config = config
 
