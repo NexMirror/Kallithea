dce727fef3a1684524c049bafacc59315650bc82
Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
2017-03-18 20:21:02 +0100

    config: move bulk of load_environment to new file app_cfg.py
    In preparation of migrating to TurboGears2, where almost all setup code is
    in one file app_cfg.py, move the existing Pylons-based configuration to the
    same location. This will make it more clear which changes are really done
    for TurboGears2.
    
    The current move does not make functional changes, the code is moved
    unmodified. Only real change is removal of some unused import statements.

--- kallithea/config/environment.py
+++ kallithea/config/environment.py
@@ -16,30 +16,10 @@
 """
 
 import os
-import logging
 import kallithea
-import platform
-
 import pylons
-import mako.lookup
-import beaker
-import formencode
 
-import kallithea.lib.app_globals as app_globals
-
-from kallithea.config.routing import make_map
-
-from kallithea.lib import helpers
-from kallithea.lib.auth import set_available_permissions
-from kallithea.lib.utils import repo2db_mapper, make_ui, set_app_settings, \
-    load_rcextensions, check_git_version, set_vcs_config, set_indexer_config
-from kallithea.lib.utils2 import engine_from_config, str2bool
-from kallithea.lib.db_manage import DbManage
-from kallithea.model.base import init_model
-from kallithea.model.scm import ScmModel
-
-log = logging.getLogger(__name__)
-
+from kallithea.config.app_cfg import setup_configuration
 
 def load_environment(global_conf, app_conf,
                      test_env=None, test_index=None):
@@ -61,79 +41,4 @@ def load_environment(global_conf, app_conf,
     # Initialize config with the basic options
     config.init_app(global_conf, app_conf, package='kallithea', paths=paths)
 
-    # store some globals into kallithea
-    kallithea.CELERY_ON = str2bool(config['app_conf'].get('use_celery'))
-    kallithea.CELERY_EAGER = str2bool(config['app_conf'].get('celery.always.eager'))
-
-    config['routes.map'] = make_map(config)
-    config['pylons.app_globals'] = app_globals.Globals(config)
-    config['pylons.h'] = helpers
-    kallithea.CONFIG = config
-
-    load_rcextensions(root_path=config['here'])
-
-    # Setup cache object as early as possible
-    pylons.cache._push_object(config['pylons.app_globals'].cache)
-
-    # Create the Mako TemplateLookup, with the default auto-escaping
-    config['pylons.app_globals'].mako_lookup = mako.lookup.TemplateLookup(
-        directories=paths['templates'],
-        strict_undefined=True,
-        module_directory=os.path.join(app_conf['cache_dir'], 'templates'),
-        input_encoding='utf-8', default_filters=['escape'],
-        imports=['from webhelpers.html import escape'])
-
-    # sets the c attribute access when don't existing attribute are accessed
-    config['pylons.strict_tmpl_context'] = True
-    test = os.path.split(config['__file__'])[-1] == 'test.ini'
-    if test:
-        if test_env is None:
-            test_env = not int(os.environ.get('KALLITHEA_NO_TMP_PATH', 0))
-        if test_index is None:
-            test_index = not int(os.environ.get('KALLITHEA_WHOOSH_TEST_DISABLE', 0))
-        if os.environ.get('TEST_DB'):
-            # swap config if we pass enviroment variable
-            config['sqlalchemy.url'] = os.environ.get('TEST_DB')
-
-        from kallithea.tests.base import TESTS_TMP_PATH
-        from kallithea.tests.fixture import create_test_env, create_test_index
-        #set KALLITHEA_NO_TMP_PATH=1 to disable re-creating the database and
-        #test repos
-        if test_env:
-            create_test_env(TESTS_TMP_PATH, config)
-        #set KALLITHEA_WHOOSH_TEST_DISABLE=1 to disable whoosh index during tests
-        if test_index:
-            create_test_index(TESTS_TMP_PATH, config, True)
-
-    # MULTIPLE DB configs
-    # Setup the SQLAlchemy database engine
-    sa_engine = engine_from_config(config, 'sqlalchemy.')
-    init_model(sa_engine)
-
-    set_available_permissions(config)
-    repos_path = make_ui('db').configitems('paths')[0][1]
-    config['base_path'] = repos_path
-    set_app_settings(config)
-
-    instance_id = kallithea.CONFIG.get('instance_id', '*')
-    if instance_id == '*':
-        instance_id = '%s-%s' % (platform.uname()[1], os.getpid())
-        kallithea.CONFIG['instance_id'] = instance_id
-
-    # CONFIGURATION OPTIONS HERE (note: all config options will override
-    # any Pylons config options)
-
-    # store config reference into our module to skip import magic of
-    # pylons
-    kallithea.CONFIG.update(config)
-    set_vcs_config(kallithea.CONFIG)
-    set_indexer_config(kallithea.CONFIG)
-
-    #check git version
-    check_git_version()
-
-    if str2bool(config.get('initial_repo_scan', True)):
-        repo2db_mapper(ScmModel().repo_scan(repos_path),
-                       remove_obsolete=False, install_git_hooks=False)
-    formencode.api.set_stdtranslation(languages=[config.get('lang')])
-    return config
+    return setup_configuration(config, paths, app_conf, test_env, test_index)
