a6f93ba4e88f774f00f777a9cc490d33beaaefe3
Mads Kiilerich <mads@kiilerich.com>
2017-10-22 23:00:37 +0200

    vcs: support Mercurial 4.4
    Review and testing shows no further problems.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ requirements = [
     "URLObject==2.3.4",
     "Routes==1.13",
     "dulwich>=0.14.1",
-    "mercurial>=2.9,<4.4",
+    "mercurial>=2.9,<4.5",
     "decorator >= 3.3.2",
     "Paste >= 2.0.3, < 3.0",
 ]
