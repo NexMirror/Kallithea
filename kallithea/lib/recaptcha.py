cde98548ad3b7b5dce363a1c8b2c4c664dfd778e
Patrick Vane <patrick_vane@lowentry.com>
2018-04-03 16:20:05 +0200

    recpatcha: Drop unused recaptcha code

--- kallithea/lib/recaptcha.py
+++ kallithea/lib/recaptcha.py
@@ -16,36 +16,6 @@ class RecaptchaResponse(object):
         return '<RecaptchaResponse:%s>' % (self.is_valid)
 
 
-def displayhtml(public_key, use_ssl=False, error=None):
-    """Gets the HTML to display for reCAPTCHA
-
-    public_key -- The public api key
-    use_ssl -- Should the request be sent over ssl?
-    error -- An error message to display (from RecaptchaResponse.error_code)"""
-
-    error_param = ''
-    if error:
-        error_param = '&error=%s' % error
-
-    if use_ssl:
-        server = API_SSL_SERVER
-    else:
-        server = API_SERVER
-
-    return """<script type="text/javascript" src="%(ApiServer)s/challenge?k=%(PublicKey)s%(ErrorParam)s"></script>
-
-<noscript>
-  <iframe src="%(ApiServer)s/noscript?k=%(PublicKey)s%(ErrorParam)s" height="300" width="500" frameborder="0"></iframe><br />
-  <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
-  <input type='hidden' name='recaptcha_response_field' value='manual_challenge' />
-</noscript>
-""" % {
-        'ApiServer': server,
-        'PublicKey': public_key,
-        'ErrorParam': error_param,
-    }
-
-
 def submit(recaptcha_challenge_field, recaptcha_response_field, private_key,
            remoteip):
     """
