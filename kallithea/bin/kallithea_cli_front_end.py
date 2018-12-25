53f33fba326de17b38b831ca68ef70feb0ebd91b
Mads Kiilerich <mads@kiilerich.com>
2018-12-25 20:23:42 +0100

    front-end: use license-checker to generate information about code used to build the front-end

--- kallithea/bin/kallithea_cli_front_end.py
+++ kallithea/bin/kallithea_cli_front_end.py
@@ -17,6 +17,7 @@ import kallithea.bin.kallithea_cli_base as cli_base
 
 import os
 import subprocess
+import json
 
 import kallithea
 
@@ -60,3 +61,23 @@ def front_end_build(install_deps, generate):
         subprocess.check_call([lesscpath, '--relative-urls', '--source-map',
                 '--source-map-less-inline', lesspath, csspath],
                 cwd=front_end_dir)
+
+        click.echo("Generating LICENSES.txt")
+        check_licensing_json_path = os.path.join(tmp_dir, 'licensing.json')
+        licensing_txt_path = os.path.join(public_dir, 'LICENSES.txt')
+        subprocess.check_call([
+            os.path.join(front_end_dir, 'node_modules', '.bin', 'license-checker'),
+            '--json',
+            '--out', check_licensing_json_path,
+            ], cwd=front_end_dir)
+        with open(check_licensing_json_path) as jsonfile:
+            rows = json.loads(jsonfile.read())
+            with open(licensing_txt_path, 'w') as out:
+                out.write("The Kallithea front-end was built using the following Node modules:\n\n")
+                for name_version, values in sorted(rows.items()):
+                    name, version = name_version.rsplit('@', 1)
+                    line = "%s from https://www.npmjs.com/package/%s/v/%s\n  License: %s\n  Repository: %s\n" % (
+                        name_version, name, version, values['licenses'], values.get('repository', '-'))
+                    if values.get('copyright'):
+                        line += "  Copyright: %s\n" % (values['copyright'])
+                    out.write(line + '\n')
