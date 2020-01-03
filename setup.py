feebfe6213bec163072da1f0aeaabe4fa8ca382c
Mads Kiilerich <mads@kiilerich.com>
2020-01-03 23:49:19 +0100

    setup: bump ipaddr minium version to 2.2.0 to get py3 support
    2.2.0 is also the latest version, but released in 2017.
    
    Previous versions failed installation on py3 with:
    
          File "/tmp/pip-install-o31ka351/ipaddr/ipaddr.py", line 1450
            ip_int = 0L
                      ^
        SyntaxError: invalid syntax
        ----------------------------------------

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ requirements = [
     "Paste >= 2.0.3, < 3.1",
     "bleach >= 3.0, < 3.2",
     "Click >= 7.0, < 8",
-    "ipaddr >= 2.1.10, < 2.3",
+    "ipaddr >= 2.2.0, < 2.3",
     "paginate >= 0.5, < 0.6",
     "paginate_sqlalchemy >= 0.3.0, < 0.4",
 ]
