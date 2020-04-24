a4e1cec1899a5c0d451ae4d48344532b05940c58
Mads Kiilerich <mads@kiilerich.com>
2020-04-24 12:42:29 +0200

    tg: clarify how ErrorController hooks into TG without using config/middleware.py

--- kallithea/controllers/error.py
+++ kallithea/controllers/error.py
@@ -41,11 +41,8 @@ log = logging.getLogger(__name__)
 class ErrorController(BaseController):
     """Generates error documents as and when they are required.
 
-    The ErrorDocuments middleware forwards to ErrorController when error
+    The errorpage middleware renders /error/document when error
     related status codes are returned from the application.
-
-    This behavior can be altered by changing the parameters to the
-    ErrorDocuments middleware in your config/middleware.py file.
     """
 
     def _before(self, *args, **kwargs):
