06e1bec52cd57c547ae0fb749c20156719524f1f
Mads Kiilerich <mads@kiilerich.com>
2020-05-10 20:50:46 +0200

    tests: coverage of Git repo feeds
    Coverage of the case reported in Issue #372.

--- kallithea/tests/functional/test_feed.py
+++ kallithea/tests/functional/test_feed.py
@@ -3,18 +3,26 @@ from kallithea.tests import base
 
 class TestFeedController(base.TestController):
 
-    def test_rss(self):
+    @base.parametrize('repo', [
+        base.HG_REPO,
+        base.GIT_REPO,
+    ])
+    def test_rss(self, repo):
         self.log_user()
         response = self.app.get(base.url(controller='feed', action='rss',
-                                    repo_name=base.HG_REPO))
+                                    repo_name=repo))
 
         assert response.content_type == "application/rss+xml"
         assert """<rss version="2.0">""" in response
 
-    def test_atom(self):
+    @base.parametrize('repo', [
+        base.HG_REPO,
+        base.GIT_REPO,
+    ])
+    def test_atom(self, repo):
         self.log_user()
         response = self.app.get(base.url(controller='feed', action='atom',
-                                    repo_name=base.HG_REPO))
+                                    repo_name=repo))
 
         assert response.content_type == """application/atom+xml"""
         assert """<?xml version="1.0" encoding="utf-8"?>""" in response
